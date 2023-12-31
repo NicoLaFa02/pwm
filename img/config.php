4ef2601198c5500aa9e90187c2de913e0a465cd9
maurisac <mauriziosacc4gmail.com>
2023-12-31 17:00:57 +0100

    Modifiche generali

--- img/config.php
+++ img/config.php
@@ -1,12 +1,20 @@
 <?php
 $host = "localhost";
-$username = "root";
-$password = "";
+$username_db = "root";
+$password_db = "";
 $database = "pwm";
 
-$conn = new mysqli($host, $username, $password, $database);
+//creazione connessione
+$conn = new mysqli($host, $username_db, $password_db, $database);
 
+//verifica connessione
 if ($conn->connect_error) {
     die("Connessione al database fallita: " . $conn->connect_error);
 }
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+
+
 ?>
