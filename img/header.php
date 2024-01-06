898de0864047dfeb552d2faef376ebc331dbe088
maurisac <mauriziosacc4gmail.com>
2024-01-06 18:40:40 +0100

    Aggiunti file Header e Footer. Header da modificare lo stile, footer da creare proprio

---
+++ img/header.php
@@ -0,0 +1,30 @@
+<!-- header.php -->
+
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RecenField</title>
+    <!-- Aggiungi eventuali stili CSS -->
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+
+    <header>
+        <div class="logo">
+            <a href="../img/">HOME</a>
+        </div>
+
+        <nav>
+            <ul>
+                <li><a href="./profilo.php">PROFILO</a></li>
+                <li><a href="./login.php">LOGIN</a></li>
+                <li><a href="./registrazione.php">REGISTRAZIONE</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <!-- Il resto del tuo contenuto HTML -->
