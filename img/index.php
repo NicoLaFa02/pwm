48e65102230ea962537a0c3634200468cd730b4a
maurisac <mauriziosacc4gmail.com>
2024-01-06 18:41:22 +0100

    aggiunto file index.php (era il vecchio file index.html)

---
+++ img/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recensioni Campi da Calcio</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+
+<div id="container">
+    <h1 id="titolo">Recensioni Campi da Calcio</h1>
+
+    <label for="ordineRecensioni">Ordina per:</label>
+    <select id="ordineRecensioni">
+        <option value="data">Data</option>
+        <option value="voto">Voto</option>
+    </select>
+
+    <form id="recensioneForm">
+        <!-- Il resto del tuo form rimane invariato -->
+    </form>
+
+    <div id="recensioni-container">
+        <!-- Il contenitore delle recensioni verrà riempito dinamicamente con JavaScript -->
+    </div>
+</div>
+
+<div id="login">
+    <form id="pulsante_login" action="../img/login.php">
+        <input type="submit" value="login">
+    </form>
+</div>
+
+<input type="text" id="searchInput" placeholder="Cerca recensioni: ...">
+
+<ul id="searchResults"></ul>
+
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script src="../js/main.js"></script>
+<script src="../js/barraric.js"></script>
+</body>
+</html>
