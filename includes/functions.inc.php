def3163e50836b59c334221d8d9b827ac033b6b9
maurisac <mauriziosacc4gmail.com>
2024-01-08 15:53:08 +0100

    aggiunte funzioni di login al file functions.inc.php

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -84,3 +84,39 @@ function createUser($conn, $username, $email, $pwd){
     header("location: ../img/registration.php?error=none");
     exit();
 }
+
+function emptyInputLogin($username, $pwd){
+    $result = null;
+    if (empty($username) || empty($pwd)){
+        $result = true;
+    }
+    else{  
+        $result = false;
+    }
+    return $result;
+}
+
+function loginUser($conn, $username, $pwd){
+    $userExists = usernameExists($conn, $username, $username);
+    //mettere di nuovo $username come 3 parametro garantisce che il controllo nel DB venga effettuato sia sulla mail che sull'username,
+    // in modo da poter effettuare l'accesso con uno o con l'altro
+    if ($userExists === false){
+        header("location: ../img/login.php?error=wronglogin");
+        exit();
+    }
+
+    $pwdHashed = $userExists['password'];
+    $checkPwd = password_verify($pwd, $pwdHashed);
+
+    if ($checkPwd === false){
+        header("location: ../img/login.php?error=incorrectpassword");
+        exit();
+    }
+    else if ($checkPwd === true){
+        session_start();
+        $_SESSION["uid"] = $userExists["uid"];
+        $_SESSION["username"] = $userExists["username"];
+        header("location: ../img/index.php");
+        exit();
+    }
+}
