9623230dbd23d690568b8a69f915535524d87d71
maurisac <mauriziosacc4gmail.com>
2024-01-08 16:17:47 +0100

    cambiata la variabile (non si collegava a quella del form per via del nome diverso)

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -2,7 +2,7 @@
 if(isset($_POST["submit"])){
     
     $username = $_POST["username"];
-    $pwd = $_POST["pwd"];
+    $pwd = $_POST["password"];
 
     require_once 'dbh.inc.php';
     require_once 'functions.inc.php';
