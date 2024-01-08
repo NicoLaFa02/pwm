b21b78b2a4157371ab5526523daf0dcfa0dd9e84
maurisac <mauriziosacc4gmail.com>
2024-01-08 21:54:06 +0100

    modifiche minori

--- img/campi.php
+++ img/campi.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="it">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Recensioni Campi da Calcio</title>
+<?php
 
-    <!-- Collega il file CSS esterno -->
-    <link rel="stylesheet" href="../css/campi.css">
-</head>
-<body>
+?>
 
 <?php
 $percorsoImmagine1 = 'https://www.messinasportiva.it/wp-content/uploads/2015/05/1capo-1280x720.jpg';
@@ -17,6 +9,15 @@ $percorsoImmagine3 = '';
 $percorsoImmagine4 = '';
 ?>
 
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/campi.css">
+</head>
+<body>
+    
 <img src="<?php echo $percorsoImmagine1; ?>" alt="Immagine 1">
 <img src="<?php echo $percorsoImmagine2; ?>" alt="Immagine 2">
 <img src="<?php echo $percorsoImmagine3; ?>" alt="Immagine 3">
@@ -27,4 +28,5 @@ $percorsoImmagine4 = '';
 
 <?php
 include_once './footer.php';
+//dovremmo fare una funzione che guarda le colonne della tabella "campoDaCalcio" e per ogni campo stampa la foto asegnata a quel campo e le informazioni, rendendo il campo accessibile per le recensioni
 ?>
