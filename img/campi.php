cf6b3f7dc558428692b71d0abc9bb4b4afb3927c
maurisac <mauriziosacc4gmail.com>
2024-01-09 00:24:19 +0100

    inserita la funzione che stampa i primi due campi dal DB

--- img/campi.php
+++ img/campi.php
@@ -1,6 +1,9 @@
 <?php
 require_once '../includes/dbh.inc.php';
 require_once '../includes/functions.inc.php';
+for ($i = 1; $i < 3; $i++) {
+    stampaCampo($conn, $i);
+}
 ?>
 
 <!DOCTYPE html>
@@ -10,17 +13,8 @@ require_once '../includes/functions.inc.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/campi.css">
 </head>
-<script>
-    // "$i" andrà sostituito con il campo con l'ultimo 
-for ($i = 1; $i < 3; $i++) {
-    stampaCampo($conn, $i);
-}</script>
 <body>
 
 </body>
 </html>
-
-<?php
-include_once './footer.php';
-//dovremmo fare una funzione che guarda le colonne della tabella "campoDaCalcio" e per ogni campo stampa la foto asegnata a quel campo e le informazioni, rendendo il campo accessibile per le recensioni
-?>
+<!-- //dovremmo fare una funzione che guarda le colonne della tabella "campoDaCalcio" e per ogni campo stampa la foto asegnata a quel campo e le informazioni, rendendo il campo accessibile per le recensioni -->
