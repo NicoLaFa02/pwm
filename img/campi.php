b0dfaeed05332a8523f45494df63f60a8552907a
NicoLaFa02 <154688075+NicoLaFa02@users.noreply.github.com>
2024-01-08 17:32:55 +0100

    Create campi.php

---
+++ img/campi.php
@@ -0,0 +1,35 @@
+<?php
+// preparare il sito per le recensioni.
+include './index.php'
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recensioni Campi da Calcio</title>
+
+    <!-- Collega il file CSS esterno -->
+    <link rel="stylesheet" href="campi.css">
+</head>
+<body>
+
+
+<?php
+$percorsoImmagine1 = 'https://www.messinasportiva.it/wp-content/uploads/2015/05/1capo-1280x720.jpg';
+$percorsoImmagine2 = '';
+$percorsoImmagine3 = '';
+$percorsoImmagine4 = '';
+?>
+
+<img src="<?php echo $percorsoImmagine1; ?>" alt="Immagine 1">
+<img src="<?php echo $percorsoImmagine2; ?>" alt="Immagine 2">
+<img src="<?php echo $percorsoImmagine3; ?>" alt="Immagine 3">
+<img src="<?php echo $percorsoImmagine4; ?>" alt="Immagine 4">
+
+
+</body>
+</html>
+
