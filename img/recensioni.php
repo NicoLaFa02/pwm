6443ce4b303b8e987d717c9289f6cdeeedb0f683
NicoLaFa02 <154688075+NicoLaFa02@users.noreply.github.com>
2024-01-10 22:52:11 +0100

    Update recensioni.php
    inserito il css per titolo e form

--- img/recensioni.php
+++ img/recensioni.php
@@ -19,22 +19,31 @@ if (!isset($_SESSION['username'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lascia una recensione</title>
-    <link rel="stylesheet" href="../css/impostazioni.css">
+    <link rel="stylesheet" href="../css/recensioni.css">
+    <style>
+    h1 {
+        text-shadow: 2px 2px green;
+    }
+</style>
 </head>
 <body>
-
-    <form action="../includes/recensioni.inc.php" method="post">Lascia una recensione
+<div id="div_titolo_rec">
+    <h1 id="titolo">Lascia una Recensione</h1>
+    </div>
+    <div id="form_recensioni">
+    <form action="../includes/recensioni.inc.php" method="post">
         
         <label for="voto">Voto:</label>
         <input type="number" id="voto" name="voto_rec" min="1" max="5" required><br><br>
-        
+        <br>
         <label for="testo_recensione">Testo della recensione:</label><br>
         <textarea id="testo" name="testo_rec" rows="4" cols="50" required></textarea><br><br>
-        
+        <br>
         <input type="hidden" name="campoID" value="<?php echo $campoID; ?>">
 
         <input type="submit" name="submit">
-
+</div>
+<br>
     </form>
 
     <!-- <a href='../includes/recensioni.inc.php?campoID=1'><button>Vai alle recensioni</button></a> -->
