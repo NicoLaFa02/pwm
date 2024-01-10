ac607912917abdda01d62f541ef9469b33ad4e03
maurisac <mauriziosacc4gmail.com>
2024-01-10 13:22:25 +0100

    cambiato stile e campi che spuntavano nel footer

--- img/footer.php
+++ img/footer.php
@@ -1,10 +1,37 @@
+<style>
+
+footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: white;
+    padding: 20px 0;
+}
+
+.footer-links {
+    display: flex;
+}
+
+.footer-links a {
+    text-decoration: none;
+    color: white;
+    margin-right: 40px;
+}
+
+.nome_sito {
+    /* display: inline; */
+    text-align: right;
+    flex-grow: 1;
+}
+
+</style>
+
 <footer>
     <div class="footer-content">
-        <p>&copy; <?php echo date("Y"); ?> RecenField</p>
-        <ul class="footer-links">
-            <li><a href="privacy.php">Privacy Policy</a></li>
-            <li><a href="termini.php">Termini di Servizio</a></li>
-            <li><a href="contatti.php">Contatti</a></li>
-        </ul>
+        <div class="footer-links">
+            <a href="http://www.github.com/NicoLaFa02/pwm">GitHub</a>
+            <a href="contatti.php">Contatti</a>
+            <div class="nome_sito">&copy; <?php echo date("Y"); ?> RecenField   c</div>
+        </div>
     </div>
 </footer>
