ea1cce7ec68ab8695b4ba9f0ecc790e0c53cdd14
maurisac <mauriziosacc4gmail.com>
2024-01-10 13:20:04 +0100

    creata sezione contatti

---
+++ img/contatti.php
@@ -0,0 +1,36 @@
+<?php
+include_once './header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contatti</title>
+    <link rel="stylesheet" href="../css/impostazioni.css">
+</head>
+<body>
+
+    <div id="informazioni-maurizio">
+        <p>Nome: Maurizio Saccà</p>
+        <p>Matricola: 504056</p>
+        <p>Email: <a href="mailto:mauriziosacc4@gmail.com">mauriziosacc4@gmail.com</p></a>
+        <p>GitHub: <a href="https://github.com/maurisac">github.com/maurisac</p></a>
+    </div>
+    
+    <br>
+    
+    <div id="informazioni-nicolò">
+        <p>Nome: Nicolò La Fauci</p>
+        <p>Matricola: 529202</p>
+        <p>Email: <a href="mailto:nico2002.nlf@gmail.com">nico2002.nlf@gmail.com</p></a>
+        <p>GitHub: <a href="https://github.com/NicoLaFa02">github.com/NicoLaFa02</p></a>
+    </div>
+
+</body>
+</html>
+
+<?php
+include_once './footer.php';
+?>
