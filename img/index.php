ccc9121c5fdd46c28a1940e53555f257a773d627
NicoLaFa02 <154688075+NicoLaFa02@users.noreply.github.com>
2024-01-10 22:53:32 +0100

    Update index.php
    inserito css per home

--- img/index.php
+++ img/index.php
@@ -1,6 +1,8 @@
 <?php
 include_once './header.php';
 if (isset($_SESSION['username'])){
+    echo '<link rel="stylesheet" type="text/css" href="styles.css">';
+
     echo '<p>Bentornato ' . $_SESSION['username'] . '</p>';
 }
 ?>
@@ -12,12 +14,20 @@ if (isset($_SESSION['username'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RecenField</title>
     <link rel="stylesheet" href="../css/style.css">
+    <style>
+    h1 {
+        text-shadow: 2px 2px green;
+    }
+</style>
 </head>
 <body>
 
 <div id="container">
+    <div id="div_titolo">
     <h1 id="titolo">Recensioni Campi da Calcio</h1>
+    </div>
 
+    
     <label for="ordineRecensioni">Ordina per:</label>
     <select id="ordineRecensioni">
         <option value="data">Data</option>
@@ -47,12 +57,12 @@ if (isset($_SESSION['username'])){
 include_once './campi.php';
 ?>
 
+<?php
+include_once './footer.php';
+?>
+
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 <script src="../js/main.js"></script>
 <script src="../js/barraric.js"></script>
 </body>
 </html>
-
-<?php
-include_once './footer.php';
-?>
