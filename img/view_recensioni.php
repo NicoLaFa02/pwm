be7ef1cccc377953a33a4f7d7257403589457b93
maurisac <mauriziosacc4gmail.com>
2024-01-11 03:24:49 +0100

    creato scheletro per scaricare le recensioni relative a un campo specifico

---
+++ img/view_recensioni.php
@@ -0,0 +1,26 @@
+<?php
+include_once './header.php';
+?>
+
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recensioni - <?php echo $campoNome ; ?></title>
+    <link rel="stylesheet" href="../css/recensioni.css">
+</head>
+<body>
+
+</body>
+</html>
+
+<?php
+include_once './footer.php';
+?>
